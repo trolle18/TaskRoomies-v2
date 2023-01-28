77853518dca894aff8fd0bb0bf250ae4aa606077
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-28 14:35:12 +0100

    init sass setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
 import './scss/main.scss'
 import App from './App';
 
