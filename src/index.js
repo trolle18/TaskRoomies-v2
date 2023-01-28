1f96f5b90d2629fd4f2f5386ebc6d50bb1e78b68
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-28 14:28:49 +0100

    sass added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
+import './scss/main.scss'
 import App from './App';
 
 
