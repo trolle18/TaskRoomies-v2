00aeb2deee38f5dc9efd1b62c41296f5f8504cb8
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-02-09 14:20:44 +0100

    edits

--- src/components/UserTasks.js
+++ src/components/UserTasks.js
@@ -11,130 +11,42 @@ import 'firebase/database';
 
 export default function UserTasks({ currentUser }) {
     const [tasks, setTasks] = useState([]); 
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [image, setImage] = useState("");
-    const [user, setUser] = useState("");
-    // const [errorMessage, setErrorMessage] = useState("");
+    // const [user, setUser] = useState("");
     const auth = getAuth();
 
-
-    // // Get current user data 
-    // useEffect(() => {
-    //     async function getUser() {
-    //     if (auth.currentUser) {
-    //         setEmail(auth.currentUser.email)
-    //         const docRef = doc(usersRef, auth.currentUser.uid)
-    //         const userData = (await getDoc(docRef)).data()  
-    //         const docSnap = await getDoc(docRef)
-
-    //         if (userData) {
-    //         setUser((prevUser) => ({ ...prevUser, ...docSnap.data() }))
-    //         setName(userData.name)
-    //         setImage(userData.image || 'placeholder')
-
-    //         setTasks(userData.userTasks)
-    //         }
-    //     }}
-    //     getUser();
-    // }, [auth.currentUser]);
-
-    // ----------------------------------------------------
-
-
-    // // Get user tasks from firebase 
-    // useEffect(() => {
-    //     async function getUserTasks() {
-    //         if (auth.currentUser) {
-    //             const docRef = doc(usersRef, auth.currentUser.uid)
-    //             const userData = (await getDoc(docRef)).data()                  
-    //             if (userData) {
-    //                 const q = query(userData.userTasks)
-    //                 const unsubscribe = onSnapshot(q, (data) => {   
-    //                     const userTasksData = data.docs.map((doc) => { return { ...doc.data(), id: doc.id } })
-    //                     setTasks(userTasksData)
-    //                 })
-    //                 return () => unsubscribe()
-    //             }
-    //     }} getUserTasks()
-    // }, [auth.currentUser]);
-
-    // ---------------------------------------------------
-
-    // const q = query(collection(db, "userTasks"), orderBy("createdAt", "desc") );
-    //     useEffect(() => {
-    //         async function getUserTasks() {
-    //             const querySnapshot = await getDocs(q);
-    //             querySnapshot.forEach((doc) => {
-    //             // doc.data() is never undefined for query doc snapshots
-    //             console.log(doc.id, " => ", doc.data());
-    //             })
-    //         } getUserTasks()
-    //     }, [])
-    //     console.log(query(collection(db, "userTasks")))
-
-    // -----------------------------------------------------
-
-
-
-    // // Get current user data 
-    // useEffect(() => {
-    //     async function getUserTasks() {
-    //         const docRef = doc(usersRef, auth.currentUser.uid)
-    //         const userData = (await getDoc(docRef)).data()  
-    //         // const docSnap = await getDoc(docRef)
-
-    //         const userTasks = collectionGroup(db, 'userTasks')
-    //         const querySnapshot = await getDocs(userTasks);
-
-    //         if (auth.currentUser) {           
-    //             if (userData) {
-    //                 // setUser((prevUser) => ( { 
-    //                 //     ...prevUser,
-    //                 //     ...docSnap.data() 
-    //                 // }))
-                    
-    //                 // querySnapshot.forEach((doc) => { console.log(doc.id, ' => ', doc.data()) })
-    //                 const tasks = userData.userTasks
-    //                 querySnapshot.forEach((doc) => { console.log( doc.data()) })
-    //                 console.log(tasks)
-    //                 setTasks(tasks)
-    //             }
-    //         } 
-    //     }
-    //     getUserTasks();
-    // }, [auth.currentUser]);
-
-        // // Get current user data 
-        // useEffect(() => {
-        //     async function getUserTasks() {
-        //         const tasksData = query(collectionGroup(db, 'userTasks'));
-        //         const querySnapshot = await getDocs(tasksData);
-        //         querySnapshot.forEach((doc) => {
-        //             console.log(doc.id, ' => ', doc.data());
-        //         });
-        //         // setTasks(tasks)
-        //     }
-        //     getUserTasks()
-        //     setTasks(tasks)
-            
-        // }, [tasks]);
-
-
-        // Gets second list from firebase
-        useEffect(() => {
-            const q = query(collectionGroup(db, 'userTasks'));
+    
+    // Get task list from firebase
+    useEffect(() => {
+        async function getTasks() {
+            const userId = await(auth?.currentUser?.uid)
+            const q = query(collectionGroup(db, 'userTasks'))
             const unsubscribe = onSnapshot(q, (data) => {    // Refers to quary instead of postRef, which returns filtered results - Unsub enables ability to watch components from a different page
-                const taskData = data.docs.map((doc) => {
-                    return { ...doc.data(), id: doc.id };   // Gets data from firebase (...doc.data) and with id: doc.id - gets the users id
-                });
-                setTasks(taskData);
-            });
-            return () => unsubscribe();
-        }, []);
+                const taskData = data.docs
+                // .filter((doc) => doc?.uid === userId )
+                .map((doc) => {
+                    return { ...doc.data(), id: doc.id, uid: doc.uid }  // Gets data from firebase (...doc.data) and with id: doc.id - gets the users id
+                })
+                console.log(taskData)
+                setTasks(taskData)
+            })
+            return () => unsubscribe()
+        }
+        getTasks()
+    }, []);
 
 
-
+//     function GetUserId() {
+//     // useEffect(() => {
+//         async function getUid() {
+//             const userId = await(auth?.currentUser?.uid)
+//             return userId
+//         }
+//         getUid()
+//         console.log("get uid: ", getUid())
+//     // }, [])
+//     }
+// GetUserId()
+    
 
     return (
         <section className="tasks-cntr">
@@ -148,7 +60,9 @@ export default function UserTasks({ currentUser }) {
                 </div>
 
                 <div className="task-posts-cntr">
-                    {tasks?.map( (task) => (
+                    {tasks
+                    // ?.filter( (task) => task.uid === GetUserId)
+                    ?.map( (task) => (
                         <TaskPost task={task} key={task.id} /> 
                     ))}
                 </div>
