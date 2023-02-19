0b527c6f31d8d7a0be41ea71b282423f24fc7acf
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-02-19 21:44:46 +0100

    --

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -6,7 +6,6 @@ import { usersRef } from "../firebase-config";
 import 'firebase/database';
 import { FaBell } from "react-icons/fa";
 import placerholder from "../assets/profile-placeholder.jpg";
-// import GroupMembers from "../components/GroupMembers";
 
 
 export default function ProfilePage({ currentUser }) {
@@ -18,7 +17,6 @@ export default function ProfilePage({ currentUser }) {
     const auth = getAuth();
     const navigate = useNavigate();
     
-
     // Get current user data 
     useEffect(() => {
         async function getUser() {
@@ -34,7 +32,7 @@ export default function ProfilePage({ currentUser }) {
             }
         }}
         getUser()
-    }, [auth.currentUser]);
+    }, [auth.currentUser])
 
 
     // Change user image
@@ -135,49 +133,7 @@ export default function ProfilePage({ currentUser }) {
                 </form>
             </div>
 
-            {/* <div className="profile-page">
-                <form>
-                    <h3>Group</h3>
-                    <GroupMembers groupMembers={groupMembers} />
-
-                     {groupMembers.map( groupMember => (          
-                    
-                        <div className="group-members-box" key={groupMember.id}>
-                            <div className="user-img">
-                                <img src={groupMember.image} alt=""/>
-                            </div>
-                            <div className="group-members-details">
-                                <p>{groupMember.name}</p>
-                                <p>{groupMember.email}</p>
-                                <input  type="text" className="group-member"  value={groupMember.name} name="name" placeholder="name"/>
-                                <input type="email"  className="group-member" value={groupMember.email} name="email" placeholder="email"/>
-                            </div>
-                            <button  className="remove-btn"> <HiMinusCircle /> </button>
-                        </div>
-                        
-                    ))} 
-
-
-                    <div className="group-members-box">
-                        <div className="user-img">
-                            <img src={image} alt="" />
-                        </div>
-                        <div className="group-members-details">
-                            <input type="text" className="group-member" value={name} name="name" placeholder="groupmember"  />
-                            <input type="email" className="group-member" value={email} name="email" placeholder="member@email.dk" />
-                        </div>
-                        <button className="remove-btn"> {" "}<HiMinusCircle />{" "}</button>
-                    </div>
-
-                    <button className="invite-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                        <path d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z" />
-                        </svg>
-                        invite
-                    </button>
-
-                </form>
-            </div> */}
+           
         </section>
     )
 };
