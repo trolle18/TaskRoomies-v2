f2361431c51f4589a325dd15594b25ba525b7060
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-02 20:26:18 +0100

    clean up

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -4,65 +4,14 @@ import { getAuth, signOut, deleteUser, EmailAuthProvider } from "firebase/auth";
 import { doc, getDoc, setDoc } from "@firebase/firestore";
 import { usersRef } from "../firebase-config";
 import 'firebase/database';
-// import { FaBell } from "react-icons/fa";
 import { BiPencil } from "react-icons/bi"
 import placerholder from "../assets/profile-placeholder.jpg";
 
 
 export default function ProfilePage({ user }) {
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [image, setImage] = useState("");
-    // const [user, setUser] = useState("");
-    // const [errorMessage, setErrorMessage] = useState("");
     const auth = getAuth();
     const navigate = useNavigate();
-    // const userId = user.id
 
-    
-    // // Get current user data 
-    // useEffect(() => {
-    //     async function getUser() {
-    //     if (auth.currentUser) {
-    //         setEmail(auth.currentUser.email)
-    //         const docRef = doc(usersRef, auth.currentUser.uid)
-    //         const userData = (await getDoc(docRef)).data()      
-    //         const docSnap = await getDoc(docRef)
-    //         if (userData) {
-    //             setUser((prevUser) => ({ ...prevUser, ...docSnap.data() }))
-    //             setName(userData.name)
-    //             setImage(userData.image || 'placeholder')
-    //         }
-    //     }}
-    //     getUser()
-    // }, [auth.currentUser])
-
-
-
-    // // Change user image
-    // function handleImageChange(event) {
-    //     const file = event.target.files[0]
-    //     if (file.size < 500000) { // image file size must be below 0,5MB
-    //         const reader = new FileReader()
-    //         reader.onload = (event) => {
-    //             setImage(event.target.result)
-    //         }
-    //         reader.readAsDataURL(file)
-    //         setErrorMessage("") // reset errorMessage state
-    //     } else { // if image >0.5MB, display an error message using the errorMessage state
-    //         setErrorMessage("The image file is too big! The image file size must be below 0,5MB")
-    //     }
-    // }
-
-
-    // // Submit updated user details
-    // async function submitEvent(event) {
-    //     event.preventDefault()
-    //     const userToUpdate = { name: name, image: image }
-    //     const docRef = doc(usersRef, auth.currentUser.uid)
-    //     await setDoc(docRef, userToUpdate)
-    //     navigate("/")
-    // }
 
 
     // Sign out
@@ -118,11 +67,6 @@ export default function ProfilePage({ user }) {
                     <div>
                         <span>Name: </span>
                         <span>{user.name}</span>
-                    </div>
-
-                    <div>
-                        <span>User id:</span>
-                        {/* <span>{auth.currentUser.uid}</span> */}
                     </div>
 
                     <div>
