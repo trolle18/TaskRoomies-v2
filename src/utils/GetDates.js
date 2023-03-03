2c3bbe1337f80d8c5cf10a720ee05c938307a9eb
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-03 20:06:23 +0100

    formatting + function

--- src/utils/GetDates.js
+++ src/utils/GetDates.js
@@ -1,5 +1,4 @@
 
-
 export function getTaskDate(task) {
     const date = task.date;
     const setDate = new Date(date).toLocaleDateString('en-GB', { month: 'short', day: '2-digit' } )
@@ -11,12 +10,16 @@ export function getTaskYear(task) {
     const year = new Date().toLocaleDateString('en-GB', { year: '2-digit' } ) 
     const taskDate = task.date;
     const taskYear = new Date(taskDate).toLocaleDateString('en-GB', { year: '2-digit' } )
-
     if(year > taskYear) return (taskYear)
-    
+};
+
+export function isOverdue(task) {
+    const today = new Date().toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' } );
+    const taskDate = task.date;
+    if(taskDate < today) return ("Overdue")
 };
 
 
-export const getTodaysDate = new Date().toLocaleDateString('en-GB', { weekday: 'long', day: '2-digit', month: 'short' } );
+export const getTodaysDate = new Date().toLocaleDateString('en-GB', { weekday: 'long',  month: 'short', day: '2-digit' } );
 
 
