6790872dc72baff09c5779e708f172a9297df33e
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-09 20:50:20 +0100

    edits

--- src/pages/UpdateProfilePage.js
+++ src/pages/UpdateProfilePage.js
@@ -85,22 +85,6 @@ export default function UpdateProfilePage() {
   }, [auth.currentUser])
 
 
-  // // Change user image
-  // function handleImageChange(event) {
-  //   const file = event.target.files[0]
-  //   if (file.size < 500000) { // image file size must be below 0,5MB
-  //     const reader = new FileReader()
-  //     reader.onload = (event) => {
-  //         setImage(event.target.result)
-  //     }
-  //     reader.readAsDataURL(file)
-  //     setErrorMessage("") // reset errorMessage state
-  //   } else { // if image >0.5MB, display an error message using the errorMessage state
-  //     setErrorMessage("The image file is too big! The image file size must be below 0,5MB")
-  //   }
-  // }
-
-
   // Submit updated user details
   async function submitEvent(event) {
     event.preventDefault()
