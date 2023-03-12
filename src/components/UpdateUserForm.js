5e183ef4a6e7ce86d80826de48067711136f26fb
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-12 21:42:09 +0100

    formatting + clean up

--- src/components/UpdateUserForm.js
+++ src/components/UpdateUserForm.js
@@ -12,57 +12,13 @@ export default function UpdateUserForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [image, setImage] = useState("");
-  // const [createdAt, setCreatedAt] = useState("");
   const [user, setUser] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const auth = getAuth();
   const navigate = useNavigate();
 
 
-  // // Get current user data
-  // useEffect(() => {
-  //   async function getUser() {
-  //   if (auth.currentUser) {
-  //     setEmail(auth.currentUser.email)
-  //     const docRef = doc(usersRef, auth.currentUser.uid)
-  //     const userData = (await getDoc(docRef)).data()
-  //     const docSnap = await getDoc(docRef)
-  //     if (userData) {
-  //       setUser((prevUser) => ({ ...prevUser, ...docSnap.data() }))
-  //       setName(userData.name)
-  //       setImage(userData.image || 'placeholder')
-  //       // setCreatedAt(userData.createdAt)
-  //     }
-  //   }}
-  //   getUser()
-  // }, [auth.currentUser])
-
-
-  // // Change user image
-  // function handleImageChange(event) {
-  //   const file = event.target.files[0]
-  //   if (file.size < 500000) { // image file size must be below 0,5MB
-  //     const reader = new FileReader()
-  //     reader.onload = (event) => {
-  //         setImage(event.target.result)
-  //     }
-  //     reader.readAsDataURL(file)
-  //     setErrorMessage("") // reset errorMessage state
-  //   } else { // if image >0.5MB, display an error message using the errorMessage state
-  //     setErrorMessage("The image file is too big! The image file size must be below 0,5MB")
-  //   }
-  // }
-
-
-  // // Submit updated user details
-  // async function submitEvent(event) {
-  //   event.preventDefault()
-  //   const userToUpdate = { name: name, image: image }
-  //   const docRef = doc(usersRef, auth.currentUser.uid)
-  //   await setDoc(docRef, userToUpdate)
-  //   navigate("/profile")
-  // }
-
+  // Get current user data
   useEffect(() => {
     async function getUser() {
     if (auth.currentUser) {
