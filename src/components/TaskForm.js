f467453f0be4079606cdb2c16fde73f5c708a4d3
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 13:42:42 +0100

    formatting

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -40,54 +40,49 @@ export default function TaskForm({ task, saveTask, handleDelete }) {
 
   return (
     <form  className="flex-cols" onSubmit={handleSubmit}>
+      <div className="flex-inner-wrapper flex-gap-2">
 
-        <div className="flex-inner-wrapper flex-gap-2">
-
-          <div className="flex-cols flex-gap-1">
-            <div className="flex-cols user-details">
-              <span className="bold">Task title</span>
-              <input
-              placeholder="Task..."
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-
-            <div className="flex-cols user-details">
-              <span className="bold">Due date</span>
-              <input
-              type="date"
-              pattern="\d{4}-\d{2}-\d{2}"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              />
-            </div>
+        <div className="flex-cols flex-gap-1">
+          <div className="flex-cols user-details">
+            <span className="bold">Task title</span>
+            <input
+            placeholder="Task..."
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
 
-     
+          <div className="flex-cols user-details">
+            <span className="bold">Due date</span>
+            <input
+            type="date"
+            pattern="\d{4}-\d{2}-\d{2}"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
 
+        <div className="flex-rows space-between">
+          <Button
+          children={ <FaRegTrashAlt/> }
+          styleType="btn btn-outline"             
+          label="Delete"
+          onClick={handleDelete}
+          disabled={false}
+          />
 
-      <div className="flex-rows space-between">
-        <Button
-        children={ <FaRegTrashAlt/> }
-        styleType="btn btn-outline"             
-        label="Delete"
-        onClick={handleDelete}
-        disabled={false}
-        />
-
-        <Button
-        children="Save"
-        type="submit"
-        styleType="btn"
-        label="Save"
-        disabled={false}
-        />
-      </div>
+          <Button
+          children="Save"
+          type="submit"
+          styleType="btn"
+          label="Save"
+          disabled={false}
+          />
+        </div>
       
       </div>
-
     </form>
   )
 };
