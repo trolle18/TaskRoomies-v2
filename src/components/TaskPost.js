99772764f80e0e21ec00dd8e613a6d23fc1877d2
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 15:48:39 +0100

    edits

--- src/components/TaskPost.js
+++ src/components/TaskPost.js
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BiPencil } from "react-icons/bi"
 import { getTaskDate, getTaskYear } from "../utils/GetDates";
-import { doc, updateDoc } from "firebase/firestore";
-import { tasksRef } from "../firebase-config";
+import { doc, getDocs, orderBy, query, updateDoc } from "firebase/firestore";
+import { tasksRef, usersRef } from "../firebase-config";
 import Checkbox from "./Checkbox";
 import Button from "./Button";
 
 
-export default function TaskPost({ task, updateUrl }) {
-  const navigate = useNavigate();
+export default function TaskPost({ task, taskType, updateUrl }) {
+  const [group, setGroup] = useState([]);
   const [check, setCheck] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
   const taskId = task.id;
+  const navigate = useNavigate();
 
 
   function handleClick() {
@@ -24,6 +25,55 @@ export default function TaskPost({ task, updateUrl }) {
     const pers = task.person
     if(pers) return ( <span className="xs-caps">{task.person}</span> );
   };
+
+
+  // Get groupmembers
+  useEffect(() => {
+    async function getGroup() {
+      const q = query(usersRef)
+      const data = await getDocs(q)
+      const groupData = data.docs.map(doc => {
+        return { ...doc.data(), id: doc.id } // changing the data structure so it's all gathered in one object
+      })
+      setGroup(groupData)
+    }
+    getGroup()
+  }, []);
+
+
+  function checkTaskType(task) {
+    const taskUid = task.uid
+    // const userUid = group.map((user) => { return (user.uid) })
+    const user = group
+    .filter((user) => user.uid === taskUid || user.name === task.person) 
+    .map((user) => { 
+      return user 
+    })
+
+    return ( 
+      <div>
+        {group
+        .filter((user) => user.uid === taskUid || user.name === task.person) 
+        .map((user) => { 
+          return (
+            <div className="img-circ-cntr">
+              <div className="img-inner-cntr">
+                <img src={user.image} alt={user.name} />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      // <div className="userimg-cntr">
+      //   <div className="user-img">
+      //     <span>{user.name}</span>
+      //     <img src={user.image} alt={user.name} />
+      //   </div>
+      // </div>
+    )
+  };
+
 
   
   useEffect(() => {
@@ -69,11 +119,12 @@ export default function TaskPost({ task, updateUrl }) {
 
             <div className="todo-text__details">
               {checkPers(task)}
+              {checkTaskType(task, taskType)}
               <span className="xs-caps">
                 {getDueDate(task)} 
               </span>
             </div>
-            
+
           </div>
         </div>
 
