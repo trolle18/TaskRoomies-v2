f705cb7def5da1ca7454dcd0f72509e9d44a4756
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 12:57:58 +0100

    formatting

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -4,71 +4,71 @@ import Button from "./Button";
 
 
 export default function TaskForm({ saveTask, task }) {
-    const [title, setTitle] = useState("");
-    const [date, setDate] = useState("");
-    const [checkBool, setCheckBool] = useState(Boolean);
-    const [type, setType] = useState("");
-    const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [checkBool, setCheckBool] = useState(Boolean);
+  const [type, setType] = useState("");
+  const navigate = useNavigate();
 
 
-    useEffect(() => {
-        if (task) {
-            setTitle(task.title)
-            setDate(task.date)
-            setCheckBool(task.checkBool)
-            setType(task.type)
-        }
-    }, [task]);
+  useEffect(() => {
+    if (task) {
+      setTitle(task.title)
+      setDate(task.date)
+      setCheckBool(task.checkBool)
+      setType(task.type)
+    }
+  }, [task]);
 
 
-    const taskData = {
-        title: title,
-        date: date,
-        checkBool: checkBool,
-        type: "usertask",
-    };
+  const taskData = {
+    title: title,
+    date: date,
+    checkBool: checkBool,
+    type: "usertask",
+  };
 
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        setType(type)
-        saveTask(taskData)
-        navigate("/")
-    };
-    
+  function handleSubmit(event) {
+    event.preventDefault()
+    setType(type)
+    saveTask(taskData)
+    navigate("/")
+  };
+  
 
-    return (
-        <form onSubmit={handleSubmit}>
+  return (
+    <form onSubmit={handleSubmit}>
 
+      <div>
+        <span>Task Title</span>
+        <input
+        placeholder="Task..."
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
 
-            <div>
-                <span>Task Title</span>
-                <input
-                placeholder="Task..."
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
+      <div>
+        <span>Due date</span>
+        <input
+        type="date"
+        pattern="\d{4}-\d{2}-\d{2}"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
 
-            <div>
-                <span>Due date</span>
-                <input
-                type="date"
-                pattern="\d{4}-\d{2}-\d{2}"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                />
-            </div>
+      <Button
+      children="Save"
+      type="submit"
+      styleType="btn"
+      label="Save"
+      disabled={false}
+      />
 
-            <Button
-            children="Save"
-            type="submit"
-            styleType="btn"
-            label="Save"
-            disabled={false}
-            />
-        </form>
-    )
+    </form>
+  )
 };
 
