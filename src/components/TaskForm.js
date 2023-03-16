2d6345bf3b682f88fa0e46ac2e6328933f35544b
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 13:21:56 +0100

    formatting and layout

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -38,35 +38,44 @@ export default function TaskForm({ saveTask, task }) {
   
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form  className="flex-cols" onSubmit={handleSubmit}>
 
-      <div>
-        <span>Task Title</span>
-        <input
-        placeholder="Task..."
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        />
+        <div className="flex-inner-wrapper">
+
+          <div className="flex-cols">
+
+            <div className="flex-cols user-details">
+              <span className="bold">Task title</span>
+              <input
+              placeholder="Task..."
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="flex-cols user-details">
+              <span className="bold">Due date</span>
+              <input
+              type="date"
+              pattern="\d{4}-\d{2}-\d{2}"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+
+
+        </div>
       </div>
 
-      <div>
-        <span>Due date</span>
-        <input
-        type="date"
-        pattern="\d{4}-\d{2}-\d{2}"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        />
-      </div>
 
-      <Button
-      children="Save"
-      type="submit"
-      styleType="btn"
-      label="Save"
-      disabled={false}
-      />
+        <Button
+        children="Save"
+        type="submit"
+        styleType="btn"
+        label="Save"
+        disabled={false}
+        />
 
     </form>
   )
