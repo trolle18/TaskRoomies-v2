1efbeb761ca6fd811e4da2e866adc3c886fb8565
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 12:30:10 +0100

    formatting

--- src/components/TaskPost.js
+++ src/components/TaskPost.js
@@ -9,83 +9,85 @@ import Button from "./Button";
 
 
 export default function TaskPost({ task, updateUrl }) {
-    const navigate = useNavigate();
-    const [check, setCheck] = useState(false);
-    const [isChecked, setIsChecked] = useState(false);
-    const taskId = task.id;
+  const navigate = useNavigate();
+  const [check, setCheck] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const taskId = task.id;
 
 
-    function handleClick() {
-        navigate(`${updateUrl}${task.id}`);
-    };
+  function handleClick() {
+    navigate(`${updateUrl}${task.id}`);
+  };
 
 
-    function checkPers(task) {
-        const pers = task.person
-        if(pers) return ( <span className="xs-caps">{task.person}</span> );
-    };
+  function checkPers(task) {
+    const pers = task.person
+    if(pers) return ( <span className="xs-caps">{task.person}</span> );
+  };
 
-    
-    useEffect(() => {
-        if (task) {
-            setCheck(task.check)           
-        }
-    }, [task]);
+  
+  useEffect(() => {
+    if (task) {
+      setCheck(task.check)           
+    }
+  }, [task]);
 
-    
+  
 
-    async function handleCheckmark(taskToUpdate) {
-        setIsChecked(!isChecked)
-        const docRef = doc(tasksRef, taskId);
-        await updateDoc(docRef, taskToUpdate); 
-        setCheck(check)
-    };
-
-
-    function getDueDate(task) {
-        const date = getTaskDate(task)
-        const year = getTaskYear(task)
-        if(task.date) return ( <> {date} {year} </> )
-        else return ("-")
-    };
+  async function handleCheckmark(taskToUpdate) {
+    setIsChecked(!isChecked)
+    const docRef = doc(tasksRef, taskId);
+    await updateDoc(docRef, taskToUpdate); 
+    setCheck(check)
+  };
 
 
-    return (
-        <>
-            <div className="task-post">
+  function getDueDate(task) {
+    const date = getTaskDate(task)
+    const year = getTaskYear(task)
+    if(task.date) return ( <> {date} {year} </> )
+    else return ("-")
+  };
 
-                <div className="checkbox-elem">
-                    <Checkbox 
-                    task={task} 
-                    handleCheckmark={handleCheckmark}
-                    />
-                </div>
 
-                <div className="todo-text-cntr">
-                    <div className="todo-text unchecked" id={`todotext${task.id}`}>
-                        <div className="todo-text__title unchecked" id="title" >
-                            <span>{task.title}</span>
-                        </div>
-                        <div className="todo-text__details">
-                            {checkPers(task)}
-                            <span className="xs-caps">
-                                {getDueDate(task)} 
-                            </span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="task-post">
 
-                <div className="edit-btn">
-                    <Button
-                    children={ <BiPencil/> }
-                    styleType="btn"              
-                    label="Edit"
-                    onClick={handleClick}
-                    disabled={false}
-                    />
-                </div>
+        <div className="checkbox-elem">
+          <Checkbox 
+          task={task} 
+          handleCheckmark={handleCheckmark}
+          />
+        </div>
 
+        <div className="todo-text-cntr">
+          <div className="todo-text unchecked" id={`todotext${task.id}`}>
+            <div className="todo-text__title unchecked" id="title" >
+              <span>{task.title}</span>
             </div>
-        </>
-    )
+
+            <div className="todo-text__details">
+              {checkPers(task)}
+              <span className="xs-caps">
+                {getDueDate(task)} 
+              </span>
+            </div>
+            
+          </div>
+        </div>
+
+        <div className="edit-btn">
+          <Button
+          children={ <BiPencil/> }
+          styleType="btn"              
+          label="Edit"
+          onClick={handleClick}
+          disabled={false}
+          />
+        </div>
+
+      </div>
+    </>
+  )
 };
