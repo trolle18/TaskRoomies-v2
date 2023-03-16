07980df27a658c33c19c88411e09dfca10e76b2e
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-03-16 20:19:48 +0100

    formatting

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -6,40 +6,45 @@ import { db } from "../firebase-config";
 
 
 export default function Checkbox({ task }) {
-    const [checkBool, setCheckBool] = useState(Boolean);
-    const taskId = task.id; 
-    const navigate = useNavigate();
-    const auth = getAuth();
+  const [checkBool, setCheckBool] = useState(Boolean);
+  const taskId = task.id; 
+  const navigate = useNavigate();
+  const auth = getAuth();
+  
 
-    async function saveTask(taskToUpdate) {
-        const uid = await(auth?.currentUser?.uid)
-        const tasksInUserRef = collection(db, `users/${uid}/userTasks/`) 
-        const docRef = doc(tasksInUserRef, taskId) 
-        await updateDoc(docRef, taskToUpdate)
-        navigate("/")
-    }
-
-    const taskData = { checkBool: checkBool }
-
-    function handleSubmit( e) {
-        setCheckBool(e.target.value)
-        e.preventDefault();        
-        saveTask(taskData);
-    }
+  async function saveTask(taskToUpdate) {
+    const uid = await(auth?.currentUser?.uid)
+    const tasksInUserRef = collection(db, `users/${uid}/userTasks/`) 
+    const docRef = doc(tasksInUserRef, taskId) 
+    await updateDoc(docRef, taskToUpdate)
+    navigate("/")
+  };
 
 
-    return (
-        <>
-            <div className="checkbox-box">
-                <input
-                type="checkbox"
-                name="checkbox"
-                id="checkbox"
-                className={`check${task.id}`}
-                checked={task.checkBool}
-                onChange={(e) => handleSubmit(e)}
-                />          
-            </div>
-        </>
-    )
+  const taskData = { 
+    checkBool: checkBool 
+  };
+
+
+  function handleSubmit( e) {
+    setCheckBool(e.target.value)
+    e.preventDefault()
+    saveTask(taskData)
+};
+
+
+  return (
+      <>
+        <div className="checkbox-box">
+          <input
+          type="checkbox"
+          name="checkbox"
+          id="checkbox"
+          className={`check${task.id}`}
+          checked={task.checkBool}
+          onChange={(e) => handleSubmit(e)}
+          />          
+        </div>
+      </>
+  )
 };
