eec7fc1d29d38754adafd3efd349db826d69b2ad
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-04-01 18:56:36 +0200

    removed unused

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,14 +1,12 @@
 import { getAuth } from "firebase/auth";
 import { collection, doc, updateDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { db, grouptaskRef } from "../firebase-config";
 
 
 export default function Checkbox({ task, taskType }) {
   const [checkBool, setCheckBool] = useState(Boolean);
   const taskId = task.id; 
-  // const navigate = useNavigate();
   const auth = getAuth();
   
 
@@ -24,27 +22,6 @@ export default function Checkbox({ task, taskType }) {
       await updateDoc(docRef, taskToUpdate)
     }
   };
-
-  
-// useEffect(() => {
-//   function ifChecked(task) {
-//     // const checked = task?.checkBool
-//     const checked = document.getElementsByClassName("checkbox")
-//     const todoTask = document.getElementsByClassName("setCheck")
-//     if(checked) {
-//       if(checked === true) {
-//         // todoTask.classList.remove("unchecked")
-//         todoTask.classList.add("checked")
-//       }
-//       else if (checked === false) { 
-//         todoTask.classList.remove("checked")
-//         // todoTask.classList.add("unchecked")
-//       }
-//     }
-//   }
-//   console.log(task.checkBool)
-//   ifChecked()
-// }, [])
 
 
   const taskData = { 
