746869d8faf5f0fd83efe29b6c0d059eeed90a13
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-04-01 14:16:49 +0200

    removed unused

--- src/components/TaskPost.js
+++ src/components/TaskPost.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BiPencil } from "react-icons/bi"
 import { getTaskDate, getTaskYear } from "../utils/GetDates";
-import { doc, getDocs, orderBy, query, updateDoc } from "firebase/firestore";
+import { doc, getDocs, query, updateDoc } from "firebase/firestore";
 import { tasksRef, usersRef } from "../firebase-config";
 import Checkbox from "./Checkbox";
 import Button from "./Button";
@@ -38,7 +38,6 @@ export default function TaskPost({ task, taskType, updateUrl }) {
   // If The task is a grouptask, add user imgs 
   function checkTaskType(task, taskType) {
     const taskUid = task.uid
-    // const pers = task.person
     if(taskType === "group") {
       return ( 
         <div>
