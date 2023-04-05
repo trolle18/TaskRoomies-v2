9289f5e5ba5d22db49512ae55f10afc830d770b5
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-04-05 23:28:04 +0200

    file structurering

---
+++ src/components/TaskForm/GroupTaskForm.js
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getDocs, query, orderBy } from "@firebase/firestore";
+import { usersRef } from "../firebase-config";
+import Button from "./Button";
+
+
+export default function GroupTaskForm({ saveGroupTask, grouptask }) {
+  const [group, setGroup] = useState([]);
+  const [title, setTitle] = useState("");
+  const [person, setPerson] = useState("");
+  const [date, setDate] = useState("");
+  const [checkBool, setCheckBool] = useState(Boolean);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (grouptask) {   
+      setTitle(grouptask.title)
+      setPerson(grouptask.person)
+      setDate(grouptask.date)
+      setCheckBool(grouptask.checkBool)
+      
+    }
+  }, [grouptask]);
+
+
+  const grouptaskData = { 
+    title: title,
+    person: person,
+    date: date,
+    checkBool: checkBool,
+  };
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    saveGroupTask(grouptaskData)
+    navigate("/")
+  };
+
+
+  // Get groupmembers
+  useEffect(() => {
+    async function getGroup() {
+      const q = query(usersRef, orderBy("name"))
+      const data = await getDocs(q)
+      const groupData = data.docs.map(doc => {
+        return { ...doc.data(), id: doc.id } // changing the data structure so it's all gathered in one object
+      })
+      setGroup(groupData)
+    }
+    getGroup()
+  }, []);
+
+
+  return (
+    <>
+      <form className="flex-cols" onSubmit={handleSubmit}>
+        <div className="flex-inner-wrapper flex-gap-2">
+
+          <div className="flex-cols flex-gap-1">
+            <div className="flex-cols">
+              <span className="bold">Task title</span>
+              <input
+              placeholder="Task..."
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="flex-cols flex-gap-0">
+              <span className="bold">Who's doing the task?</span>
+              <select value={person} onChange={(e) => setPerson(e.target.value)} >
+                <option>Choose</option>
+                <option value="fælles">Fælles</option>
+                {group.map(person => (
+                  <option value={person.uid} key={person.uid}>{person.name}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex-cols">
+              <span className="bold">Due date</span>
+              <input
+              type="date"
+              pattern="\d{4}-\d{2}-\d{2}"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              />
+            </div>  
+          </div>
+
+          <div className="flex-rows space-between">
+            <Button
+            children="Save"
+            type="submit"
+            styleType="btn"
+            label="Save"
+            disabled={false}
+            />
+          </div>
+
+        </div>
+      </form>
+    </>
+  )
+};
+
---
+++ src/components/TaskForm/TaskForm.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaRegTrashAlt } from "react-icons/fa";
+import Button from "../Button";
+
+
+export default function TaskForm({ task, saveTask, handleDelete }) {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [checkBool, setCheckBool] = useState(Boolean);
+  const [type, setType] = useState("");
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (task) {
+      setTitle(task.title)
+      setDate(task.date)
+      setCheckBool(task.checkBool)
+      setType(task.type)
+    }
+  }, [task]);
+
+
+  const taskData = {
+    title: title,
+    date: date,
+    checkBool: checkBool,
+    type: "usertask",
+  };
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setType(type)
+    saveTask(taskData)
+    navigate("/")
+  };
+  
+
+  return (
+    <form  className="flex-cols" onSubmit={handleSubmit}>
+      <div className="flex-inner-wrapper flex-gap-2">
+
+        <div className="flex-cols flex-gap-1">
+          <div className="flex-cols">
+            <span className="bold">Task title</span>
+            <input
+            placeholder="Task..."
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+
+          <div className="flex-cols">
+            <span className="bold">Due date</span>
+            <input
+            type="date"
+            pattern="\d{4}-\d{2}-\d{2}"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="flex-rows space-between">
+          {/* <Button
+          children={ <FaRegTrashAlt/> }
+          styleType="btn btn-outline"             
+          label="Delete"
+          onClick={handleDelete}
+          disabled={false}
+          /> */}
+
+          <Button
+          children="Save"
+          type="submit"
+          styleType="btn"
+          label="Save"
+          disabled={false}
+          />
+        </div>
+      
+      </div>
+    </form>
+  )
+};
+
